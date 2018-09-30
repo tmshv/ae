17b094e926487f46143ddb2fe9be7dd07971ade2
Roman Timashev <roman@tmshv.ru>
2018-09-30 17:45:08 +0300

    lib tslinted

--- src/components/Aeditor/lib.ts
+++ src/components/Aeditor/lib.ts
@@ -1,22 +1,22 @@
-import { Change } from 'slate';
-import { getEventTransfer, getEventRange } from 'slate-react';
+import { Change } from 'slate'
+import { getEventTransfer, getEventRange } from 'slate-react'
 
-import isImage from 'is-image';
-import isUrl from 'is-url';
-import { BlockType } from './const';
+import isImage from 'is-image'
+import isUrl from 'is-url'
+import { BlockType } from './const'
 // import insertImage from './insertImage';
 // import insertVideo from './insertVideo';
 // import insertLink from './insertLink';
 
 export function handleTextPaste(e, change) {
-    const target = getEventRange(e, change.value);
+    const target = getEventRange(e, change.value)
 
-    const transfer = getEventTransfer(e);
-    const { text } = transfer;
+    const transfer = getEventTransfer(e)
+    const { text } = transfer
     // if (!isUrl(text)) return null;
 
     if (isImage(text)) {
-        return change.call(insertImage, text, target);
+        return change.call(insertImage, text, target)
     }
     //  else if (text.match(/youtube\.com|vimeo\.com/)) {
     //     return change.call(insertVideo, text);
@@ -27,7 +27,7 @@ export function handleTextPaste(e, change) {
 
 export function insertImage(change: Change, src: string, target) {
     if (target) {
-        change.select(target, null)
+        change.select(target, undefined)
     }
 
     return change.insertBlock({
