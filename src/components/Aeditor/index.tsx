a15384df428aaf2fe773cbb710d2edbeff458b4d
Roman Timashev <roman@tmshv.ru>
2018-10-04 20:43:35 +0300

    renderNode refactoring

--- src/components/Aeditor/index.tsx
+++ src/components/Aeditor/index.tsx
@@ -1,25 +1,15 @@
 import React from 'react'
 import { Editor, getEventTransfer } from 'slate-react'
 import { Value, Change } from 'slate'
-import EditList from 'slate-edit-list'
-import isUrl from 'is-url'
 import blockquotePlugin from './plugins/blockquote'
 import listPlugin from './plugins/list'
 import tablePlugin from './plugins/table'
 import schema from './schema'
 import { handleTextPaste } from './lib'
 
-import { BlockType } from './const'
-
-import imagePasteDrop from './plugins/imagePasteDrop'
-import { Paragraph } from './blocks/Paragraph'
-import { Image } from './blocks/Image'
-import { File } from './blocks/File'
-import { Blockquote } from './blocks/Blockquote'
-
 import Toolbar from '../Toolbar'
-import { Header } from './blocks/Header'
 import exitHeader from './plugins/exitHeader'
+import renderNode from './renderNode'
 
 import './styles.less'
 
@@ -167,90 +157,6 @@ export default class Aeditor extends React.Component<AeditorProps, State> {
         }
     }
 
-    renderNode = props => {
-        const { node, attributes } = props
-
-        switch (node.type) {
-            case BlockType.header1: {
-                return (
-                    <Header {...props} />
-                )
-            }
-
-            case BlockType.paragraph: {
-                return (
-                    <Paragraph {...props} />
-                )
-            }
-
-            case BlockType.image: {
-                return (
-                    <Image {...props} />
-                )
-            }
-
-            case BlockType.file: {
-                return (
-                    <File {...props} />
-                )
-            }
-
-            case BlockType.blockquote: {
-                return (
-                    <Blockquote {...attributes}>{props.children}</Blockquote>
-                )
-            }
-
-            case BlockType.listItem: {
-                return (
-                    <li {...attributes}>{props.children}</li>
-                )
-            }
-
-            case BlockType.orderedList: {
-                return (
-                    <ol {...attributes}>{props.children}</ol>
-                )
-            }
-
-            case BlockType.unorderedList: {
-                return (
-                    <ul {...attributes}>{props.children}</ul>
-                )
-            }
-
-            case BlockType.table: {
-                return (
-                    <table>
-                        <tbody {...attributes}>
-                            {props.children}
-                        </tbody>
-                    </table>
-                )
-            }
-
-            case BlockType.tableRow: {
-                return (
-                    <tr {...attributes}>
-                        {props.children}
-                    </tr>
-                )
-            }
-
-            case BlockType.tableCell: {
-                return (
-                    <td {...attributes}>
-                        {props.children}
-                    </td>
-                )
-            }
-
-            default: {
-                break
-            }
-        }
-    }
-
     renderToolbar() {
         return (
             <Toolbar
@@ -266,9 +172,9 @@ export default class Aeditor extends React.Component<AeditorProps, State> {
                 onPaste={this.onPaste}
                 value={this.state.value}
                 onChange={this.onChange}
-                renderNode={this.renderNode}
+                renderNode={renderNode}
+                schema={schema}
                 // onKeyDown={this.onKeyDown}
-                // schema={schema}
                 // renderEditor={renderEditor}
                 plugins={plugins}
             />
---
+++ src/components/Aeditor/renderNode.tsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import { BlockType } from './const'
+import { Paragraph } from './blocks/Paragraph'
+import { Image } from './blocks/Image'
+import { File } from './blocks/File'
+import { Blockquote } from './blocks/Blockquote'
+import { Header } from './blocks/Header'
+
+export default function renderNode(props) {
+    const { node, attributes } = props
+
+    switch (node.type) {
+        case BlockType.header1: {
+            return (
+                <Header {...props} />
+            )
+        }
+
+        case BlockType.paragraph: {
+            return (
+                <Paragraph {...props} />
+            )
+        }
+
+        case BlockType.image: {
+            return (
+                <Image {...props} />
+            )
+        }
+
+        case BlockType.file: {
+            return (
+                <File {...props} />
+            )
+        }
+
+        case BlockType.blockquote: {
+            return (
+                <Blockquote {...attributes}>{props.children}</Blockquote>
+            )
+        }
+
+        case BlockType.listItem: {
+            return (
+                <li {...attributes}>{props.children}</li>
+            )
+        }
+
+        case BlockType.orderedList: {
+            return (
+                <ol {...attributes}>{props.children}</ol>
+            )
+        }
+
+        case BlockType.unorderedList: {
+            return (
+                <ul {...attributes}>{props.children}</ul>
+            )
+        }
+
+        case BlockType.table: {
+            return (
+                <table className={'ae-block-table'}>
+                    <tbody {...attributes}>
+                        {props.children}
+                    </tbody>
+                </table>
+            )
+        }
+
+        case BlockType.tableRow: {
+            return (
+                <tr {...attributes}>
+                    {props.children}
+                </tr>
+            )
+        }
+
+        case BlockType.tableCell: {
+            return (
+                <td {...attributes}>
+                    {props.children}
+                </td>
+            )
+        }
+
+        default: {
+            break
+        }
+    }
+}
