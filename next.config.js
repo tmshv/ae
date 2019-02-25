62eb89f821c45ce848092f50982ea580194a696c
Roman Timashev <roman@tmshv.ru>
2019-02-25 18:03:04 +0300

    next config updated to load css

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const withTypescript = require('@zeit/next-typescript')
 const withLess = require('@zeit/next-less')
+const withCss = require('@zeit/next-css')
 
-module.exports = withTypescript(withLess())
+module.exports = withTypescript(withLess(withCss()))
