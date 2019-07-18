9a03638f1c0a6cace5c519fe7f83ed8dd23fa0f0
Roman Timashev <roman@tmshv.ru>
2019-07-18 19:06:07 +0300

    Ae lib schema PARENT_TYPE_INVALID fix

--- src/core/Ae/lib/schema.ts
+++ src/core/Ae/lib/schema.ts
@@ -1,9 +1,10 @@
 import { Change, Block } from 'slate'
+import { PARENT_TYPE_INVALID } from 'slate-schema-violations'
 
 export function normalizeParentTypeInvalid(change: Change, blockType: string) {
     normalize: (change: Change, violation: string, { node }: { node: Block }) => {
         switch (violation) {
-            case 'PARENT_TYPE_INVALID': {
+            case PARENT_TYPE_INVALID: {
                 return change.wrapBlockByKey(node.key, blockType)
             }
 
