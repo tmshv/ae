38d4a6581ab6b9c129c79f185c8a561d14b95a1d
Roman Timashev <roman@tmshv.ru>
2019-07-18 22:24:27 +0300

    createFigure typing

--- src/core/Ae/utils/createFigure.ts
+++ src/core/Ae/utils/createFigure.ts
@@ -1,7 +1,8 @@
 import { Block } from 'slate'
 import { BlockType } from '../const'
+import { List } from 'immutable'
 
-export default function createFigure(nodes): Block {
+export default function createFigure(nodes: List<Block>): Block {
     return Block.create({
         type: BlockType.figure,
         nodes,
