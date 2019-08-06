c0ae86ca28f74b8f423640386ea97f0347c5ee79
Roman Timashev <roman@tmshv.ru>
2019-08-06 17:41:37 +0300

    BlockProps interface update

--- src/core/Ae/blocks/index.ts
+++ src/core/Ae/blocks/index.ts
@@ -1,11 +1,12 @@
 import * as React from 'react'
 
 export interface BlockProps {
+    className?: string
     // node: slate.Node,
-    node: any,
-    attributes: object,
-    isSelected: boolean,
-    children: React.ReactNode,
+    node: any
+    attributes: object
+    isSelected: boolean
+    children: React.ReactNode
 
     editor: any
 }
