17591234af742e69a15d6ed65e7416259d61ac9a
Roman Timashev <roman@tmshv.ru>
2022-12-06 01:20:44 +0600

    port to const

--- src/server.js
+++ src/server.js
@@ -117,9 +117,10 @@ app.prepare()
             return handle(req, res)
         })
 
-        server.listen(3000, (err) => {
+        const port = 3000
+        server.listen(port, (err) => {
             if (err) throw err
-            console.log('> Ready on http://localhost:3000')
+            console.log(`> Ready on http://localhost:${port}`)
         })
     })
     .catch((ex) => {
